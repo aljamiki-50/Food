c64a0d3cf98a87965c36995ae67a63ccb6228aae
aljamiki-50 <almazin77@yahoo.com>
2023-12-08 16:52:08 +0000

    adding a video secon with workin button

--- food-landingpage/src/container/Intro/Intro.jsx
+++ food-landingpage/src/container/Intro/Intro.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from "react";
+import { BsFillPlayFill, BsPauseFill, BsPlayFill } from "react-icons/bs";
+import { meal } from "../../constants";
+import "./Intro.css";
+import { useState } from "react";
 
-import './Intro.css';
+const Intro = () => {
+  const [playvideo, setPlayvideo] = useState(false);
+  const videRef = React.useRef();
+  const handleVid = () => {
+    setPlayvideo((prevplayvideo) => !prevplayvideo);
+    if (videRef) {
+      videRef.current.play();
+    } else {
+      videRef.current.pause();
+    }
+  };
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+  return (
+    <div className="app__video">
+      <video src={meal} type="video/mp4" loop ref={videRef} controls={false} />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={handleVid}
+        >
+          {playvideo ? (
+
+            <BsPauseFill color="#fff" fontSize={30} />
+          ): <BsPlayFill color="#fff" fontSize={30} />}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
