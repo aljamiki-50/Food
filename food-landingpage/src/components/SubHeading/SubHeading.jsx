5e1ee3bdf58a8f3789f0ac88e0e41ab2859a5aaf
aljamiki-50 <almazin77@yahoo.com>
2023-12-08 13:13:24 +0000

    adding and styling s enue - and make it responsice

--- food-landingpage/src/components/SubHeading/SubHeading.jsx
+++ food-landingpage/src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { images } from "../../constants";
 
-const SubHeading = () => (
-  <div>
-    SubHeading
+const SubHeading = ({title}) => (
+  <div style={{margin: '1rem'}}>
+    <p className="p__cormorant">{title}</p>
+    <p className="p__cormorant"></p>
+    <img src={images.spoon} alt="images"  className='spoon__img'/>
   </div>
 );
 
--- food-landingpage/src/container/Chef/Chef.jsx
+++ food-landingpage/src/container/Chef/Chef.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import {images  } from "../../constants";
+import {SubHeading  } from "../../components";
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='`' >
+   
   </div>
 );
 
