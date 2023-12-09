4c1eec27190c69cc05c24faa64a0c31a8d371a70
aljamiki-50 <almazin77@yahoo.com>
2023-12-09 13:04:57 +0000

    adding the award sect within media

--- food-landingpage/src/container/Intro/Intro.jsx
+++ food-landingpage/src/container/Intro/Intro.jsx
@@ -7,27 +7,34 @@ import { useState } from "react";
 const Intro = () => {
   const [playvideo, setPlayvideo] = useState(false);
   const videRef = React.useRef();
+
   const handleVid = () => {
     setPlayvideo((prevplayvideo) => !prevplayvideo);
-    if (videRef) {
-      videRef.current.play();
-    } else {
-      videRef.current.pause();
+
+    const videoElement = videRef.current;
+
+    if (videoElement) {
+      if (playvideo) {
+        videoElement.pause();
+      } else {
+        videoElement.play();
+      }
     }
   };
 
   return (
     <div className="app__video">
-      <video src={meal} type="video/mp4" loop ref={videRef} controls={false} />
+      <video src={meal} type="video/mp4" loop autoPlay ref={videRef} controls={false} />
       <div className="app__video-overlay flex__center">
         <div
           className="app__video-overlay_circle flex__center"
           onClick={handleVid}
         >
           {playvideo ? (
-
             <BsPauseFill color="#fff" fontSize={30} />
-          ): <BsPlayFill color="#fff" fontSize={30} />}
+          ) : (
+            <BsPlayFill color="#fff" fontSize={30} />
+          )}
         </div>
       </div>
     </div>
